b6890bfc4d204179ace764c2bd948fc1bac2fe17
Alex Kanunnikov <lifeart92@gmail.com>
2019-06-24 20:55:25 +0300

    jsx extraction tests (#13)
    * jsx extraction tests
    
    * entry node lookup fixes
    
    * tests fix
    
    * more tests
    
    * tests update
    
    * declaration extraction
    
    * test fix
    
    * handy context handing
    
    * interesting case

--- src/utils/informator.ts
+++ src/utils/informator.ts
@@ -291,22 +291,180 @@ export function extractComponentInformationFromMeta(meta: any) {
 }
 
 var extractedComponents = {};
+var declarations = {};
+
+function hasValidJSXEntryNode(item) {
+	return item && (item.type === 'JSXElement' || item.type === 'JSXFragment');
+}
+
+function addDeclarations(content, declarations) {
+	let finalResult = content;
+	let hashes = [];
+	let contextName = 'ctx';
+	Object.keys(declarations).forEach((key)=>{
+		let value = declarations[key];
+		if (key.startsWith('this.')) {
+			finalResult = finalResult.split(key).join(value);
+		} else {
+			finalResult = finalResult.split('this.' + key).join(`${contextName}.${key}`);
+			if (typeof value === 'string') {
+				hashes.push(`${key}="${value}"`);
+			} else if (typeof value === 'number') {
+				hashes.push(`${key}=${value}`);
+			} else if (typeof value === 'boolean') {
+				hashes.push(`${key}=${value}`);
+			}
+		}
+	});
+	if (hashes.length === 0) {
+		return finalResult;
+	}
+	let output = `{{#let (hash ${hashes.join(' ')}) as |ctx|}}${finalResult}{{/let}}`;
+	return output;
+}
+
+function addComponent(name, content) {
+	let uniqName = name;
+	if (name in extractedComponents) {
+		uniqName = uniqName + '_' + Math.random().toString(36).slice(-6);
+	}
+	extractedComponents[uniqName] = content;
+}
 
 function jsxComponentExtractor() {
   extractedComponents = {};
+ declarations = {};
   return {
     FunctionDeclaration(path) {
       let node = path.node;
-      if (node.id.name && node.body && node.body.body.length) {
+      if (node.id && node.id.name && node.body && node.body.body && node.body.body.length) {
         let result = node.body.body.filter((el)=>el.type === 'ReturnStatement');
         if (result.length) {
           const arg = result[0].argument;
-          if (arg.type === 'JSXElement' || arg.type === 'JSXFragment') {
-            extractedComponents[node.id.name] = print(cast(arg, result[0]));
+          if (hasValidJSXEntryNode(arg)) {
+			addComponent(node.id.name, print(cast(arg, result[0])));
           }
         }
       }
-    }
+	},
+    FunctionExpression(path) {
+      let node = path.node;
+      if (node.body && node.body.body && node.body.body.length) {
+        let result = node.body.body.filter((el)=>el.type === 'ReturnStatement');
+        if (result.length) {
+          const arg = result[0].argument;
+          if (hasValidJSXEntryNode(arg)) {
+			addComponent('FunctionExpression', print(cast(arg, result[0])));
+          }
+        }
+      }
+	},
+	ClassMethod(path) {
+		let node = path.node;
+		if (node.key.name && node.body && node.body.body && node.body.body.length) {
+		  let result = node.body.body.filter((el)=>el.type === 'ReturnStatement');
+		  if (result.length) {
+			const arg = result[0].argument;
+			if (hasValidJSXEntryNode(arg)) {
+				addComponent(node.key.name, print(cast(arg, result[0])));
+			}
+		  }
+		}
+	},
+	VariableDeclarator(path) {
+		let node = path.node;
+		if (node.id && node.id.type === 'Identifier') {
+			if (node.init) {
+				if (node.init.type === 'StringLiteral') {
+					declarations[node.id.name] =  node.init.value;
+				} else if (node.init.type === 'NumericLiteral') {
+					declarations[node.id.name] =  node.init.value;
+				} else if (node.init.type === 'BooleanLiteral') {
+					declarations[node.id.name] =  node.init.value;
+				}
+			}
+		} else if (node.id && node.id.type === 'ArrayPattern') {
+			if (node.init && node.init.type === 'CallExpression') {
+				if (node.init.callee.type === 'Identifier' && node.init.callee.name === 'useState') {
+					node.id.elements.forEach((el, index)=>{
+						if (el.type === 'Identifier') {
+							if (node.init.arguments[index]) {
+								const argType = node.init.arguments[index].type;
+								if (['StringLiteral', 'NumericLiteral', 'BooleanLiteral'].includes(argType)) {
+									declarations[el.name] =   node.init.arguments[index].value;
+								}
+							}
+						}
+					});
+				}
+			}
+		} else if (node.id && node.id.type === 'ObjectPattern') {
+			if (node.init && node.init.type === 'MemberExpression') {
+				if (node.init.object.type === 'ThisExpression') {
+					if (node.init.property.type === 'Identifier') {
+						if (node.init.property.name === 'props') {
+							node.id.properties.forEach((prop)=>{
+								if (prop.key.type === 'Identifier') {
+									declarations['this.' + prop.key.name] = '@' + prop.key.name;
+								}
+							});
+						}
+					}
+				}
+			}
+		}
+		if (node.id && node.id.name && node.init) {
+			if (hasValidJSXEntryNode(node.init)) {
+				addComponent(node.id.name, print(cast(node.init, node)));
+			}
+		}
+	},
+	ArrowFunctionExpression(path) {
+		let node = path.node;
+
+		if (node.params && node.params.length === 1  && node.params[0].type === 'ObjectPattern') {
+			node.params[0].properties.forEach((prop)=>{
+				if (prop.type === 'ObjectProperty') {
+					if (prop.key.type === 'Identifier') {
+						declarations['this.' + prop.key.name] = '@' + prop.key.name;
+					}
+				}
+			});
+		}
+		if (node.body) {
+			if (hasValidJSXEntryNode(node.body)) {
+				addComponent('ArrowFunctionExpression', print(cast(node.body, node)));
+			} else if (node.body.body && node.body.body.length) {
+				let result = node.body.body.filter((el)=>el.type === 'ReturnStatement');
+				if (result.length) {
+					const arg = result[0].argument;
+					if (hasValidJSXEntryNode(arg)) {
+						addComponent('ArrowFunctionExpression', print(cast(arg, result[0])));
+					}
+        		}
+			}
+		}
+	},
+	ObjectMethod(path) {
+		let node = path.node;
+		if (node.key.name && node.body && node.body.body && node.body.body.length) {
+		  let result = node.body.body.filter((el)=>el.type === 'ReturnStatement');
+		  if (result.length) {
+			const arg = result[0].argument;
+			if (hasValidJSXEntryNode(arg)) {
+				addComponent(node.key.name, print(cast(arg, result[0])));
+			}
+		  }
+		}
+	},
+	ObjectProperty(path) {
+		let node = path.node;
+		if (node.key.name && node.value) {
+			if (hasValidJSXEntryNode(node.value)) {
+				addComponent(node.key.name, print(cast(node.value, node)));
+			}
+		}
+	}
   }
 }
 
@@ -316,5 +474,13 @@ export function extractJSXComponents(jsxInput) {
     parserOpts: { isTSX: true }
   });
   traverse(ast, jsxComponentExtractor());
+
+  Object.keys(extractedComponents).forEach((componentName)=>{
+	const declarated = addDeclarations(extractedComponents[componentName], declarations);
+	if (extractedComponents[componentName] !== declarated) {
+		extractedComponents[componentName + '_declarated'] = declarated;
+	}
+  });
+
   return extractedComponents;
 }
--- test/utils/informator.test.js
+++ test/utils/informator.test.js
@@ -119,6 +119,64 @@ it("can extract simple JSX components from file", () => {
 	});
 });
 
+it("can extract jsx from ClassMethod", () => {
+	const input = `
+	import React from 'react';
+	import WithEmberSupport from 'ember-react-components';
+
+
+	export default class BasicComponent extends React.Component {
+		render() {
+			return <h1>Hello from React</h1>;
+		}
+	}
+	`;
+
+	assert(extractJSXComponents(input), {
+		'render': '<h1>Hello from React</h1>'
+	});
+});
+it("can extract jsx from VariableDeclarator", () => {
+	const input = `
+	var data = <h1>Hello from React</h1>;
+	`;
+
+	assert(extractJSXComponents(input), {
+		'data': '<h1>Hello from React</h1>'
+	});
+});
+it("can extract jsx from ArrowFunctionExpression", () => {
+	const input = `
+	var data = () => <h1>Hello from React</h1>;
+	`;
+
+	assert(extractJSXComponents(input), {
+		'ArrowFunctionExpression': '<h1>Hello from React</h1>'
+	});
+});
+it("can extract jsx from FunctionExpression", () => {
+	const input = `
+	function node() {}
+
+node(function () {
+	return <div></div>;
+});
+	`;
+
+	assert(extractJSXComponents(input), {
+		'FunctionExpression': '<div></div>'
+	});
+});
+it("can extract jsx from ArrowFunctionExpression, using return", () => {
+	const input = `
+	var data = () => { return <h1>Hello from React</h1> };
+	`;
+
+	assert(extractJSXComponents(input), {
+		'ArrowFunctionExpression': '<h1>Hello from React</h1>'
+	});
+});
+
 it('can extract component, returning fragment', () => {
 	const input = `function FragmentedComponent(props) {
 		return (<><div></div><div></div></>);
@@ -126,8 +184,125 @@ it('can extract component, returning fragment', () => {
 	assert(extractJSXComponents(input), {
 		FragmentedComponent: "<div></div><div></div>"
 	});
-})
+});
 
+it('can extract component from object method', () => {
+	const input = `
+	let ob = {
+		name() {
+			return <div></div>;
+		}
+	};
+	`;
+	assert(extractJSXComponents(input), {
+		name: "<div></div>"
+	});
+});
+it('can extract component from object property', () => {
+	const input = `
+	let ob = {
+		name:  <div></div>
+	};
+	`;
+	assert(extractJSXComponents(input), {
+		name: "<div></div>"
+	});
+});
+it('can handle basic string declarations', () => {
+	// todo fix fails
+	const input = `
+	function App() {
+		const greeting = "Hi";
+	  
+		return <h1>{greeting}</h1>;
+	  }
+	`;
+	assert(extractJSXComponents(input), {
+		App: "<h1>{{this.greeting}}</h1>",
+		App_declarated:  "{{#let (hash greeting=\"Hi\") as |ctx|}}<h1>{{ctx.greeting}}</h1>{{/let}}"
+	});
+});
+it('can handle basic numeric declarations', () => {
+	// todo fix fails
+	const input = `
+	function App() {
+		const greeting = 42;
+	  
+		return <h1>{greeting}</h1>;
+	  }
+	`;
+	assert(extractJSXComponents(input), {
+		App: "<h1>{{this.greeting}}</h1>",
+		App_declarated:  "{{#let (hash greeting=42) as |ctx|}}<h1>{{ctx.greeting}}</h1>{{/let}}"
+	});
+});
+it('can handle spread as arguments for arrow function', () => {
+	const input = `
+	const Headline = ({ value }) => {
+		return <h1>{value}</h1>;
+	  };
+	`;
+	assert(extractJSXComponents(input), {
+		ArrowFunctionExpression: "<h1>{{this.value}}</h1>",
+		ArrowFunctionExpression_declarated: "<h1>{{@value}}</h1>",
+	});
+	// App.. = <h1>{{@value}}</h1>
+});
+it('can handle spread as arguments for named function', () => {
+	const input = `
+	function Headline({ value }) {
+		return <h1>{value}</h1>;
+	  };
+	`;
+	assert(extractJSXComponents(input), {
+		Headline: "<h1>{{this.value}}</h1>"
+	});
+
+	// App.. = <h1>{{@value}}</h1>
+
+	// same for
+	//  const { name } = this.props;
+    //return (
+	//	<p>Hello, {name}</p>
+	//  );
+});
+it('can handle spread for this.props', () => {
+	const input = `
+	function Headline() {
+		const { value } = this.props;
+		return <h1>{value}</h1>;
+	  };
+	`;
+	assert(extractJSXComponents(input), {
+		Headline: "<h1>{{this.value}}</h1>",
+		Headline_declarated: "<h1>{{@value}}</h1>"
+	});
+});
+it('can handle components with state hook', () => {
+	const input = `
+	const Headline = () => {
+		const [greeting, setGreeting] = useState(
+		  'Hello Function Component!'
+		);
+	  
+		const handleChange = event => setGreeting(event.target.value);
+	  
+		return (
+		  <div><h1>{greeting}</h1><input type="text" value={greeting} onChange={handleChange} /></div>
+		);
+	  };
+	`;
+	// {{#let (hash greeting="hello") as |ctx|}} 
+	//   {{let (hash updateGreeting=(action (mut ctx.greeting) value="target.value")) as |act|}}
+	//      <input {{on 'change' act.updateGreeting}}>
+	//   {{/let}}
+	// {{/let}}
+	// idea todo -> we can catch setGreeting and produce {{action (mut this.greeting)}}, or kinda
+	assert(extractJSXComponents(input), {
+		ArrowFunctionExpression: "<div><h1>{{this.greeting}}</h1><input type=\"text\" value={{this.greeting}} {{on \"change\" this.handleChange}} /></div>",
+		ArrowFunctionExpression_declarated: "{{#let (hash greeting=\"Hello Function Component!\") as |ctx|}}<div><h1>{{ctx.greeting}}</h1><input type=\"text\" value={{ctx.greeting}} {{on \"change\" this.handleChange}} /></div>{{/let}}"
+	});
+});
 function assert(left, right) {
   expect(left).toEqual(right);
 }
