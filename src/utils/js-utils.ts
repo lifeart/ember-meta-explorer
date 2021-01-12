2b63913f258b4fa27025f4e050ba61a34175389b
Alex Kanunnikov <lifeart92@gmail.com>
2021-01-12 11:53:47 +0300

    update deps (#21)
    * update deps
    
    * more babel plugins

--- src/utils/js-utils.ts
+++ src/utils/js-utils.ts
@@ -310,7 +310,13 @@ const babelOptions = {
     ["@babel/proposal-class-properties", { loose: true }],
     "@babel/proposal-object-rest-spread",
     "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-numeric-separator",
+    "@babel/plugin-proposal-logical-assignment-operators",
+    "@babel/plugin-proposal-private-methods",
+    "@babel/plugin-proposal-async-generator-functions",
+    "@babel/plugin-proposal-function-sent",
+    "@babel/plugin-proposal-do-expressions"
   ],
   sourceType: "module",
   // parserOpts: {
